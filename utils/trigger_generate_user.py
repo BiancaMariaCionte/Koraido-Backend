70d41c232489f5166759efd30bee67339fcd726f
Bianca-Maria Cionte <147051212+BiancaMariaCionte@users.noreply.github.com>
2025-05-21 20:18:13 +0300

    Update trigger_generate_user.py

--- utils/trigger_generate_user.py
+++ utils/trigger_generate_user.py
@@ -1,7 +1,7 @@
 import requests
 
 def trigger_generate_user(firebase_uid):
-    url = 'http://192.168.0.196:5000/generate'
+    url = 'http://192.168.0.195:5000/generate'
     payload = {'firebase_uid': firebase_uid}
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, json=payload, headers=headers)
